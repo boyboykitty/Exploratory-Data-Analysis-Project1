5cca6189dee20048f559d939fb0869db9cf1c11f
boyboykitty <boyboykitty@hotmail.com>
2015-07-13 06:52:57 +0800

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -2,16 +2,20 @@
 library(dplyr)
 # load date
 mydata<-read.table("./data/household_power_consumption.txt", header = T,sep=";",nrows =70000,stringsAsFactors =FALSE)
+## Converting dates
 mydata$Date<- as.Date(strptime(mydata$Date, "%d/%m/%Y"))
 
 # filter data
 mydata<-filter(mydata,Date=='2007-2-1'|Date=='2007-2-2')
 
-#plot2
+## Converting dates and data to numeric
 datetime<-strptime(paste(mydata$Date,mydata$Time),"%Y-%m-%d %H:%M:%S")
 globalActivePower <- as.numeric(mydata$Global_active_power)
-Sys.setlocale(,"C")  # show the date as English date 
 
+ # show the date as English date 
+Sys.setlocale(,"C") 
+
+# plot2
 png('plot2.png',width=480,height=480)
 plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
 dev.off()
